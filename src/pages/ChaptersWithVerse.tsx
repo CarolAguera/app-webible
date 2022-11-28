71d8a5336ca7fe57fb89122f28d879c966736cf1
Carolina Aguera <carolaguerabr@gmail.com>
2022-11-28 13:34:28 -0300

    feat: add new page chapters with verses

---
+++ src/pages/ChaptersWithVerse.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header/Header";
+import ChapterWithVerses from "../components/ChapterWithVerses/ChapterWithVerses";
+import React from "react";
+
+export default function Chapters() {
+  return (
+    <React.Fragment>
+      <Header />
+      <ChapterWithVerses />
+    </React.Fragment>
+  );
+}
